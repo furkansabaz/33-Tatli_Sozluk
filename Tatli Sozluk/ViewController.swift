996d39652f4b6077afcad8e0ef563127e3480e29
Furkan Sabaz <furkan.sabaz@gmail.com>
2019-06-21 21:28:21 +0300

    Initial Commit

---
+++ Tatli Sozluk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tatli Sozluk
+//
+//  Created by Furkan Sabaz on 21.06.2019.
+//  Copyright © 2019 Furkan Sabaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
