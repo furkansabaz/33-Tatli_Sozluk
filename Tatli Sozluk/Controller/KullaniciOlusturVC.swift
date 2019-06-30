e85a24c9021cf0d9dba4188c8b09c9a0950674b4
Furkan Sabaz <furkan.sabaz@gmail.com>
2019-06-30 10:59:39 +0300

    Ders 14-Authentication Giriş

--- Tatli Sozluk/Controller/KullaniciOlusturVC.swift
+++ Tatli Sozluk/Controller/KullaniciOlusturVC.swift
@@ -7,14 +7,13 @@
 //
 
 import UIKit
-
+import Firebase
+import FirebaseAuth
 class KullaniciOlusturVC: UIViewController {
 
     
     @IBOutlet weak var txtEmailAdresi: UITextField!
-    
     @IBOutlet weak var txtParola: UITextField!
-    
     @IBOutlet weak var btnVazgectim: UIButton!
     @IBOutlet weak var txtKullaniciAdi: UITextField!
     @IBOutlet weak var btnHesapOlustur: UIButton!
@@ -28,9 +27,51 @@ class KullaniciOlusturVC: UIViewController {
     }
     
     @IBAction func btnHesapOlusturPressed(_ sender: Any) {
+        
+        guard let emailAdresi = txtEmailAdresi.text,
+        let parola = txtParola.text,
+            let kullaniciAdi = txtKullaniciAdi.text else { return }
+        
+        Auth.auth().createUser(withEmail: emailAdresi, password: parola) { (kullaniciBilgileri , hata) in
+            
+            if let hata = hata {
+                debugPrint("Kullanıcı Oluşturulurken Hata Meydana Geldi \(hata.localizedDescription)")
+            }
+            // Hata meydana gelmedi o zaman kullanıcı başarılı bir şekilde oluşturuldu
+            
+            let changeRequest = kullaniciBilgileri?.user.createProfileChangeRequest()
+            changeRequest?.displayName = kullaniciAdi
+            changeRequest?.commitChanges(completion: { (hata) in
+                
+                if let hata = hata {
+                    debugPrint("Kullanıcı Adı Güncellenirken Hata Meydana Geldi : \(hata.localizedDescription)")
+                }
+            })
+            
+            guard let kullaniciId = kullaniciBilgileri?.user.uid else { return }
+            
+            Firestore.firestore().collection(KULLANICILAR_REF).document(kullaniciId).setData([
+                KULLANICI_ADI : kullaniciAdi,
+                KULLANICI_OLUSTURMA_TARIHI : FieldValue.serverTimestamp()
+                ], completion:  { (hata) in
+                    if let hata = hata {
+                        debugPrint("Kullanıcı Eklenirken Hata Meydana Geldi \(hata.localizedDescription)")
+                    } else {
+                        self.dismiss(animated: true, completion: nil)
+                    }
+                    
+            })
+            
+            
+            
+        }
+        
+        
+        
     }
     
     @IBAction func btnVazgectimPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
     
 }
--- Tatli Sozluk/Model/Sabitler.swift
+++ Tatli Sozluk/Model/Sabitler.swift
@@ -16,3 +16,8 @@ let Begeni_Sayisi = "BegeniSayisi"
 let Fikir_Text = "FikirText"
 let Kullanici_Adi = "KullaniciAdi"
 let Eklenme_Tarihi = "EklenmeTarihi"
+
+
+let KULLANICILAR_REF = "Kullanicilar"
+let KULLANICI_ADI = "KullaniciAdi"
+let KULLANICI_OLUSTURMA_TARIHI = "OlusturulmaTarihi"
